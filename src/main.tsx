6d9d13e7293499bbc5d568a9bff6fec3de433f95
Kate V <72123212+kattyv@users.noreply.github.com>
2022-08-17 12:54:08 +0300

    gh-pages settings chanjes

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ''}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
